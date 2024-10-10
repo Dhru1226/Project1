6bb525767c88edfc6f0307b07a6452012406e459
dhrumi <dhrumi2006@gmail.com>
2024-10-10 15:54:58 -0400

    Add project files.

---
+++ Project1.cpp
@@ -0,0 +1,99 @@
+/*
+Title: Project 1
+Filename: Project1
+Programmer: Dhrumi Mistry
+Date:10/10/2024
+Requirements:
+Crazy Al’s Computer Emporium is a retail seller of home computers. The sales staff at Crazy Al’s works strictly on commission. At the end of the month, each salesperson’s commission
+is calculated according to Table 1.
+Table 1 Commission rates
+Sales This Month Commission Rate
+Less than $10,000 5%
+$10,000–$14,999 10%
+$15,000–$17,999 12%
+$18,000–$21,999 14%
+$22,000 or more 16%
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    //calculate commission and return commission rate
+    double calculateCommissionrate(double sales) {
+        if (sales < 10000)
+            return 0.05;
+        else if (sales >= 10000 && sales <= 14999)
+            return 0.10;
+        else if (sales >= 15000 && sales <= 17999)
+            return 0.12;
+        else if (sales >= 18000 && sales <= 21999)
+            return 0.14;
+        else
+            return 0.16;
+
+    }
+    void displayResults(double sales, double commissionrate, double commission, double advancePay) {
+        double remainpay = commission - advancePay;
+
+        cout << fixed << setprecision(2);
+        cout << "Pay Results" << endl;
+        cout << "----------" << endl;
+        cout << "sales: $" << sales << endl;
+        cout << "Commission rate : " << commissionRate << endl;
+        cout << "Commission: $" << commission << endl;
+        cout << "Advanced Pay: $" << advancePay << endl;
+
+        if (remainingPay >= 0)
+            cout << "Remaining Pay: $" << remaingPay << endl;
+        else
+            cout << "Amount to reimburse Crazy Al's: $" << -remainingPay << endl;
+    }
+
+    int main() {
+        double sales, advancePay;
+
+        //Input sales and advance pay
+        cout << "Enter the salsperson'smonthly sales: ";
+        cin >> sales;
+
+        cout << "Enter the amount of advanced pay for this salesperson: ";
+        cin >> advancePay;
+
+        //calculate
+        double commissionRate = calculateCommissionrate(sales);
+        double commission = sales * commissionrate;
+
+        //Display the results
+        displayresults(sales, commissionrate, commission, advancePay);
+
+        return 0;
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
